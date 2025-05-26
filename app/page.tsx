1a455575d734551353684e36d8718dd48c6aae9a
Uros Stojnic <ustojnic@gmail.com>
2025-05-26 20:21:14 +0200

    Added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,23 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className="text-red-400">Home page</h1>
       </main>
+      
+
+ <nav className="bg-gray-400 p-14 shadow w-full fixed top-0 left-0 right-0"  style={{ padding: "20px 40px" }}>
+        <ul className="flex justify-center" style={{ marginLeft: "20px", marginRight: "20px" }}>
+          <li className="text-white hover:text-blue-300" style={{ marginRight: "50px" }}>
+            Home
+          </li>
+          <li className="text-white hover:text-blue-300" style={{ marginRight: "50px" }}>
+            About Us
+          </li>
+          <li className="text-white hover:text-blue-300">
+            Contact
+          </li>
+        </ul>
+      </nav>
+
     </div>
   );
 }
+
