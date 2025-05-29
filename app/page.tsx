01ae4c7457a6aea36059550ede1b32a3b61b483f
Uros Stojnic <ustojnic@gmail.com>
2025-05-29 15:19:55 +0200

    Odradjen responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,39 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+
+      <main className={`${styles.main} pt-32`}>
         <h1 className="text-red-400">Home page</h1>
       </main>
-      
 
- <nav className="bg-background p-6 shadow w-full fixed top-0 left-0 right-0">
-        <ul className="flex justify-center mr-5 ml-5">
-          <li className="text-white hover:text-blue-300 mr-12">
-            Home
-          </li>
-          <li className="text-white hover:text-blue-300 mr-12">
-            About Us
-          </li>
-          <li className="text-white hover:text-blue-300">
-            Contact
-          </li>
-        </ul>
-      </nav>
+      <header className="bg-background p-6 shadow w-full fixed top-0 left-0 right-0 z-50">
+        <div className="relative mx-auto flex max-w-screen-lg flex-col sm:flex-row sm:items-center sm:justify-center">
+          
+          {/* Hamburger dugme */}
+          <input className="peer hidden" type="checkbox" id="navbar-toggle" />
+          <label htmlFor="navbar-toggle" className="absolute right-4 top-0 cursor-pointer text-white text-2xl sm:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 448 512">
+              <path fill="currentColor" d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z" />
+            </svg>
+          </label>
 
+          <nav className="peer-checked:block hidden sm:block mt-4 sm:mt-0 w-full sm:w-auto text-center">
+            <ul className="flex flex-col sm:flex-row sm:space-x-10 text-white text-lg">
+              <li className="hover:text-blue-300">
+                <a href="#">Home</a>
+              </li>
+              <li className="hover:text-blue-300">
+                <a href="#">About Us</a>
+              </li>
+              <li className="hover:text-blue-300">
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </div>
   );
 }
-
